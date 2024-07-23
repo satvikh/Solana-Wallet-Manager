d81a14bb866e7632199bf171cb9bb4980dc0f7de
satvikh <satvikh05@gmail.com>
2024-07-23 03:39:04 -0500

    rudimentary wallet generation

--- walletFunctions.js
+++ walletFunctions.js
@@ -1,3 +1,36 @@
-function generateWallet(){
+//generates x amount of wallets on the solana blockchain and stores the private keys in storedWallets.json
+
+
+function generateWallets(num){
+    if (typeof num === 'number'){
+
+        storedWallets.json[walletGroup]=
+        const mnemonic = bip39.generateMnemonic();
+        storedWallets.json['mnemonic']=mnemonic
+
+        for (let i=0; i<num; i++){
+            
+            
+            //Generating a wallet name
+            let walletName = `wallet ${i}`;
+            
+
+            
+
+            //Storing wallet information in .json file
+
+           
+            //Confirming successful wallet generation
+            console.log(`${walletName} generated successfully`);
+        }
+
+    }
     
 }
+
+//checks the balance of all wallets in storedWallets.json
+function checkWallets(){
+
+}
+
+

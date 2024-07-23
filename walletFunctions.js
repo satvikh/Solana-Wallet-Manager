bc27d1224e6cf8f2e7f5948ad6fb33e2db73c5ff
satvikh <satvikh05@gmail.com>
2024-07-23 03:57:24 -0500

    finished the "generateMnemonic" and "generateWallets" function

--- walletFunctions.js
+++ walletFunctions.js
@@ -1,13 +1,30 @@
+//generates a seedPhrase can be used to generate multiple wallets
+function generateMnemonic(){
+    const mnemonic = bip39.generateMnemonic();
+    storedWallets.json[mnemonic] = mnemonic;
+}
+
+
 //generates x amount of wallets on the solana blockchain and stores the private keys in storedWallets.json
-
-
 function generateWallets(num){
     if (typeof num === 'number'){
+        const mnemonic = storedWallets.json[mnemonic];
 
-        storedWallets.json[walletGroup]=
-        const mnemonic = bip39.generateMnemonic();
-        storedWallets.json['mnemonic']=mnemonic
 
+        const seed = bip39.mnemonicToSeedSync(mnemonic, ""); // (mnemonic, password)
+        for (let i = 0; i < num+1; i++) {
+            const path = `m/44'/501'/${i}'/0'`;
+            const keypair = Keypair.fromSeed(derivePath(path, seed.toString("hex")).key);
+
+            storedWallets.json[`wallet${i}`]={[keypair.publicKey] : keypair};
+            console.log(`${path} => ${keypair.publicKey.toBase58()}`);
+}
+
+
+
+
+
+        //Generating wallets
         for (let i=0; i<num; i++){
             
             
